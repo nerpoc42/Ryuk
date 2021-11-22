bf1ed9a5c29411e36c97b1e596e8ef0a234dab22
Nerijus Pocevičius <nerpoc42@gmail.com>
2021-11-22 23:14:00 +0200

    Fixed image finding hopefully, and higher resolution

--- src/food.py
+++ src/food.py
@@ -9,10 +9,44 @@ from settings import settings
 
 url = "https://www.facebook.com/kubosvetaine"
 
+def is_correct_img(img):
+	return "p526x296" in img['src']
+
+def get_higher_res_img(img):
+    parent = img.parent
+    print(parent.name)
+    while parent.name != 'a':
+        parent = parent.parent
+
+    link = "https://facebook.com"+parent['href']
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = settings['chrome_install_location']
+    options.add_argument('--headless')
+    # options.add_argument('--lang=lit')
+    driver_location = settings['chrome_driver_location']
+    driver = webdriver.Chrome(driver_location, chrome_options=options)
+    driver.set_window_size(1920,1080)
+
+    driver.get(link)
+
+    time.sleep(5)
+
+    html = driver.page_source
+
+    driver.close()
+
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    
+    imgs = soup.find_all('img')
+
+    return imgs[0]
+
 def get_food():
     options = webdriver.ChromeOptions()
     options.binary_location = settings['chrome_install_location']
     options.add_argument('--headless')
+    # options.add_argument('--lang=lit')
     driver_location = settings['chrome_driver_location']
     driver = webdriver.Chrome(driver_location, chrome_options=options)
     driver.set_window_size(1920,1080)
@@ -34,15 +68,21 @@ def get_food():
     # print('test')
     imgs = soup.find_all('img')
     # print(imgs)
-    # for img in soup.find_all('img', {'class': ['i09qtzwb', 'n7fi1qx3', 'datstx6m', 'pmk7jnqg', 'j9ispegn', 'kr520xx4', 'k4urcfbm', 'bixrwtb6']}):
-    # print(imgs[-1]['src'])
+
+    driver.close()
+
+    for img in imgs:
+        if 'p480x480' in img['src']:
+            return get_higher_res_img(img)['src']
+     
     # for img in imgs:
     #     # print(img.string)
     #     # print(str(img.text))
     #     print(img['src'])
 
-    driver.close()
+    return None    
 
-    img = imgs[-1]['src']
+    # for img in imgs:
+    #     if is_correct_img(img):
+    #         return img
 
-    return img
